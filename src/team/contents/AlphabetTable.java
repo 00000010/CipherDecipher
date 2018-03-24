96a0030a9b24d45ef4daa28d79b0dc3f00986d3b
jessicaaxelson <37740669+jessicaaxelson@users.noreply.github.com>
2018-03-24 17:20:07 -0400

    Added user stuff, decryption

--- src/team/contents/AlphabetTable.java
+++ src/team/contents/AlphabetTable.java
@@ -4,36 +4,93 @@ import java.util.Hashtable;
 
 public class AlphabetTable {
 
-	static Hashtable<String, Integer> table;
+	static Hashtable<String, Integer> inputTable;
+	static Hashtable<Integer, String> outputTable;
 	
-	public static void init_table() {
-		table = new Hashtable<String, Integer>();
-		table.put("A", 0);
-		table.put("B", 1);
-		table.put("C", 2);
-		table.put("D", 3);
-		table.put("E", 4);
-		table.put("F", 5);
-		table.put("G", 6);
-		table.put("H", 7);
-		table.put("I", 8);
-		table.put("J", 9);
-		table.put("K", 10);
-		table.put("L", 11);
-		table.put("M", 12);
-		table.put("N", 13);
-		table.put("O", 14);
-		table.put("P", 15);
-		table.put("Q", 16);
-		table.put("R", 17);
-		table.put("S", 18);
-		table.put("T", 19);
-		table.put("U", 20);
-		table.put("V", 21);
-		table.put("W", 22);
-		table.put("X", 23);
-		table.put("Y", 24);
-		table.put("Z", 25);
+	public static void initInputTable() {
+		inputTable = new Hashtable<String, Integer>();
+		inputTable.put("A", 0);
+		inputTable.put("a", 0);
+		inputTable.put("B", 1);
+		inputTable.put("b", 1);
+		inputTable.put("C", 2);
+		inputTable.put("c", 2);
+		inputTable.put("D", 3);
+		inputTable.put("d", 3);
+		inputTable.put("E", 4);
+		inputTable.put("e", 4);
+		inputTable.put("F", 5);
+		inputTable.put("f", 5);
+		inputTable.put("G", 6);
+		inputTable.put("f", 6);
+		inputTable.put("H", 7);
+		inputTable.put("h", 7);
+		inputTable.put("I", 8);
+		inputTable.put("i", 8);
+		inputTable.put("J", 9);
+		inputTable.put("j", 9);
+		inputTable.put("K", 10);
+		inputTable.put("k", 10);
+		inputTable.put("L", 11);
+		inputTable.put("l", 11);
+		inputTable.put("M", 12);
+		inputTable.put("m", 12);
+		inputTable.put("N", 13);
+		inputTable.put("n", 13);
+		inputTable.put("O", 14);
+		inputTable.put("o", 14);
+		inputTable.put("P", 15);
+		inputTable.put("p", 15);
+		inputTable.put("Q", 16);
+		inputTable.put("q", 16);
+		inputTable.put("R", 17);
+		inputTable.put("r", 17);
+		inputTable.put("S", 18);
+		inputTable.put("s", 18);
+		inputTable.put("T", 19);
+		inputTable.put("t", 19);
+		inputTable.put("U", 20);
+		inputTable.put("u", 20);
+		inputTable.put("V", 21);
+		inputTable.put("v", 21);
+		inputTable.put("W", 22);
+		inputTable.put("w", 22);
+		inputTable.put("X", 23);
+		inputTable.put("x", 23);
+		inputTable.put("Y", 24);
+		inputTable.put("y", 24);
+		inputTable.put("Z", 25);
+		inputTable.put("z", 25);
+	}
+	
+	public static void initOutPutTable() {
+		outputTable = new Hashtable<Integer, String>();
+		outputTable.put(0, "A");
+		outputTable.put(1, "B");
+		outputTable.put(2, "C");
+		outputTable.put(3, "D");
+		outputTable.put(4, "E");
+		outputTable.put(5, "F");
+		outputTable.put(6, "G");
+		outputTable.put(7, "H");
+		outputTable.put(8, "I");
+		outputTable.put(9, "J");
+		outputTable.put(10, "K");
+		outputTable.put(11, "L");
+		outputTable.put(12, "M");
+		outputTable.put(13, "N");
+		outputTable.put(14, "O");
+		outputTable.put(15, "P");
+		outputTable.put(16, "Q");
+		outputTable.put(17, "R");
+		outputTable.put(18, "S");
+		outputTable.put(19, "T");
+		outputTable.put(20, "U");
+		outputTable.put(21, "V");
+		outputTable.put(22, "W");
+		outputTable.put(23, "X");
+		outputTable.put(24, "Y");
+		outputTable.put(25, "Z");
 	}
 		
 	/**
@@ -42,7 +99,10 @@ public class AlphabetTable {
 	 * @return
 	 */
 	public static int getHashValue(String string) {
-		return table.get(string);
+		return inputTable.get(string);
+	}
+	public static String translateValue(int number) {
+		return outputTable.get(number);
 	}
 	
 }
--- src/team/contents/application/Decipher.java
+++ src/team/contents/application/Decipher.java
@@ -9,27 +9,55 @@ public class Decipher extends AlphabetTable {
 	
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
-		AlphabetTable.init_table();
+		AlphabetTable.initInputTable();
+		AlphabetTable.initOutPutTable();
 		
 		while(true) {										//can have input after input						//ask for input
-			System.out.println("Enter"); 					//can input string
-			String input = scanner.nextLine(); 				//reads input
+			System.out.println("");
+			System.out.println("Encrypt or decrypt?"); 					//can input string
+			String answerInput = scanner.nextLine(); 				//reads input
 			
-			String[] newInput = input.split("");
-			
-			for (int i = 0; i < input.length(); i++) {
-//				try {
-					String letter = newInput[i];
-					int hashNumber = AlphabetTable.getHashValue(letter);
-					System.out.println(hashNumber);
-//				}
-//				catch (Exception e) {
-//					System.out.println("You messed up");
-//				}
-			}	
-		}
-		
-	}
-	
+			if (answerInput.equals("E") || answerInput.equals("e") || answerInput.equals("Encrypt") || answerInput.equals("encrypt") || answerInput.equals("Encrypted please")) {
+				System.out.print("Type in what you'd like to encrypt: ");
+				String stringEncryptInput = scanner.nextLine();
+				String[] encryptArray = stringEncryptInput.split("");
+				
+				for (int i = 0; i < stringEncryptInput.length(); i++) {
+					String letter = encryptArray[i];
+					if (letter.equals(",") || letter.equals(" ") || letter.equals("!") || letter.equals("?") || letter.equals("'")) {
+				      	System.out.print(encryptArray[i]);
+				    }
+					else {
+						int hashNumber = AlphabetTable.getHashValue(letter);
+						String decryption = AlphabetTable.translateValue(hashNumber + 1);
+						System.out.print(decryption);
+					}
+					//System.out.println(hashNumber);
+				}
+			}
+			else if (answerInput.equals("D") || answerInput.equals("d") || answerInput.equals("Decrypt") || answerInput.equals("decrypt") || answerInput.equals("Decrypted please")){
+				System.out.println("Type in what you'd like to decrypt: ");
+				String stringDecryptInput = scanner.nextLine();
+				String[] decryptArray = stringDecryptInput.split("");
+				
+				for (int i = 0; i < stringDecryptInput.length(); i++) {
+					String letter = decryptArray[i];
+					if (letter.equals(",") || letter.equals(" ") || letter.equals("!") || letter.equals("?") || letter.equals("'") || letter.equals("\"") || letter.equals(".")) {
+				      	System.out.print(decryptArray[i]);
+				    }
+					else {
+						int hashNumber = AlphabetTable.getHashValue(letter);
+						String decryption = AlphabetTable.translateValue(hashNumber - 1);
+						System.out.print(decryption);
+					}
+					//System.out.println(hashNumber);
+				}
+			}
+			else {
+				System.out.println("You have entered an invalid input. Please try again.");
+			}
+		}	
+	}		
 }
+	
 
